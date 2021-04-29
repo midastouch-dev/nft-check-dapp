5f65cc2fc68278874289722a526bf57762ee76ea
tylerfyu <tylerfyu@outlook.com>
2021-04-29 15:18:54 +0000

    basic dapp

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Container, Form, InputGroup, Tooltip, Button, OverlayTrigger } from 'react-bootstrap';
+import logo from './logo.svg';
+import './App.css';
+import { connectMetaMask } from './connect';
+
+function App() {
+  const [account, setAccount] = React.useState<string>('');
+  const [balance, setBalance] = React.useState<string>('');
+  const [network, setNetwork] = React.useState<string>('Moonbase Alpha');
+  const [busy, setBusy] = React.useState<boolean>(false);
+  const [isConnected, setConnected] = React.useState<boolean>(false);
+
+  async function connect(toAlpha?: boolean) {
+    setBusy(true);
+    const { networkId, isConnected } = await connectMetaMask(
+      (accounts: string[]) => {
+        setAccount(accounts[0]);
+        // updateBalance(accounts[0]);
+      },
+      async (_networkId: number) => {
+        // await updateBalance(account);
+        // setNetwork(networkName(_networkId));
+      },
+      toAlpha
+    );
+    setConnected(isConnected)
+    // setNetwork(networkName(networkId));
+    setBusy(false);
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="warning" block size="sm" disabled={busy} onClick={() => connect()}>
+          <small>Connect</small>
+        </Button>
+        <div>{isConnected?"Connected":"Not Connected"}</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/connect.ts
@@ -0,0 +1,79 @@
+import Web3 from 'web3';
+import { AbiItem, hexToNumber } from 'web3-utils';
+import detectEthereumProvider from '@metamask/detect-provider';
+
+type Address = string;
+export 	async function connectMetaMask(
+		onAccountsChanged: (accounts: Address[]) => void,
+		onNetworkChanged: (networkId: number) => void,
+		toAlpha?: boolean
+	) {
+        let isConnected:boolean=false
+        let web3:Web3
+        let networkId:number=0
+		if ((window as { [key: string]: any }).ethereum) {
+			console.log('Last updated: 04/27/21');
+			// const { ethereum } = window as { [key: string]: any };
+			const provider: any = await detectEthereumProvider({ mustBeMetaMask: true });
+			console.log('PROVIDER', provider);
+			if (provider && provider.isMetaMask) {
+				try {
+					// initiate web3
+					web3 = new Web3(provider);
+					// this.web3 = web3;
+
+					// Enable MetaMask accounts
+					const accountsRead = await provider.request({ method: 'eth_requestAccounts' });
+					// const accountsRead = await web3.eth.getAccounts();
+					if (toAlpha) {
+						await provider.request({
+							method: 'wallet_addEthereumChain',
+							params: [
+								{
+									chainId: '0x507',
+									chainName: 'Moonbase Alpha',
+									nativeCurrency: {
+										name: 'DEV',
+										symbol: 'DEV',
+										decimals: 18,
+									},
+									rpcUrls: ['https://rpc.testnet.moonbeam.network'],
+									blockExplorerUrls: ['https://moonbase-blockscout.testnet.moonbeam.network/'],
+								},
+							],
+						});
+					}
+
+					if (onAccountsChanged) {
+						onAccountsChanged(accountsRead);
+						provider.on('accountsChanged', async (accounts: string[]) => {
+							if (accounts.length > 0) {
+								const accountsReadAgain = await provider.request({ method: 'eth_accounts' });
+								// const accountsReadAgain = await web3.eth.getAccounts();
+								console.log('accountsChanged', accountsReadAgain);
+								onAccountsChanged(accountsReadAgain);
+							} else {
+								window.location.reload();
+							}
+						});
+
+						provider.on('chainChanged', async (chainId: string) => {
+							console.log('chainChanged', chainId, hexToNumber(chainId));
+							onNetworkChanged(Number(chainId));
+							await connectMetaMask(onAccountsChanged, onNetworkChanged);
+						});
+					}
+					isConnected = true;
+                    networkId = await web3.eth.net.getId();
+
+				} catch (e) {
+					if (e.code !== 4001) {
+						throw new Error(e.message);
+					}
+				}
+			} else {
+				throw new Error('Other ethereum wallet did not support.');
+			}
+		}
+		return { isConnected, networkId };
+	}
