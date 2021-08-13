3ea5e1ea0bb7ad194aecb353fcf3fcb77da1f607
tylerfyu <tylerfyu@outlook.com>
2021-08-13 14:51:42 +0200

    UI and text changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { InputGroup, Button, Card, FormControl } from "react-bootstrap";
+import { InputGroup, Button, Card, FormControl, Container } from "react-bootstrap";
 import { checkNFTOwnership } from "./methods/checkNFTOwership";
 import { connectMetaMask } from "./methods/connect";
 import { signEmail } from "./methods/signVerify";
@@ -75,7 +75,7 @@ function App() {
         >
           <div className="navbar-brand">
             <a className="navbar-item">
-              <img src={mrLogo} alt="Moonriver" />
+              <img src={mrLogo} alt="Moonriver" style={{maxHeight: 50}}/>
             </a>
           </div>
           <div className="navbar-end">
@@ -89,41 +89,36 @@ function App() {
             </a>
           </div>
         </nav>
-        <section className="section">
+
+        <Container>
           <Card body>
             <Card.Title>
-              Step 1: Connect Wallet to Check Moonbeam PolkaPets Ownership
+              Step 1: Connect MetaMask to Check Moonbeam PolkaPets Ownership
             </Card.Title>
             <Card.Text>
-              Moonbeam PolkaPet owners are invited to join the Moonbeam Take
-              Flight token event whitelist. Please note Chinese and U.S.
-              citizens and residents are not eligible to participate in this
-              token event.
+              Moonbeam PolkaPet owners are invited to join the Moonbeam Take Flight token event whitelist.<br />
+              Please note Chinese and U.S. citizens and residents are <u>not eligible</u> to participate in this token event.
+              For additional information on eligibility please see <a href="https://moonbeam.foundation/take-flight/#eligibility" target="_blank" rel="noopener">Take Flight eligibility</a>.
             </Card.Text>
-            <Button
-              variant="warning"
-              block
-              size="sm"
-              disabled={busy}
-              onClick={() => connect()}
-            >
-              <small>Connect and Check</small>
-            </Button>
-            <div
-              style={
-                connectionStatus === "Connected"
-                  ? { color: "green" }
-                  : { color: "red" }
-              }
-            >
-              {connectionStatus}
-            </div>
-            <div>Account: {account}</div>
-            {connectionStatus === "Connected" && (
-              <div style={hasNFT ? { color: "green" } : { color: "red" }}>
-                {hasNFT ? "Owns a MOONBEAM NFT" : "Doesn't Own a MOONBEAM NFT"}
+
+            {connectionStatus === "Connected" && 
+              <div>Account: {account} - <span style={hasNFT ? { color: "green" } : { color: "red" }}>
+                  {hasNFT ? "Owns a MOONBEAM NFT" : "This account doesn't own a MOONBEAM NFT"}
+                </span>
               </div>
-            )}
+            }
+
+            {connectionStatus === "Not Connected" && 
+              <Button
+                variant="info"
+                block
+                size="sm"
+                disabled={busy}
+                onClick={() => connect()}
+              >
+                <small>Connect and Check</small>
+              </Button>
+            }
           </Card>
           {hasNFT ? (
             <Card body>
@@ -134,39 +129,53 @@ function App() {
               <Card.Text>
                 The email address you provide must be the same one you will use
                 to register for the Take Flight event. By providing your email,
-                you are subscribing to email notifications from the Moonbeam
-                Foundation about the event.
+                you are subscribing to email notifications from the Moonbeam Foundation
+                and you are agreeing to the terms and conditions of
+                the Moonbeam Foundation’s Privacy Policy outlined <a href="https://moonbeam.foundation/privacy-policy/" target="_blank" rel="noopener">here</a>. 
               </Card.Text>
-              <InputGroup className="mb-3">
-                <InputGroup.Text id="basic-addon1">Email</InputGroup.Text>
-                <FormControl
-                  placeholder="email@domain.com"
-                  value={email}
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                />
-              </InputGroup>
-              <Button
-                variant="warning"
-                block
-                size="sm"
-                disabled={busy}
-                onClick={() => associateAndSend()}
-              >
-                <small>Sign And Send</small>
-              </Button>
-              <div>Signature: {signature}</div>
-              <div
-                style={
-                  status === "Submited successfully" ? { color: "green" } : {}
-                }
-              >
-                Status: {status}
-              </div>
+
+              { signature.length === 0 ?
+                <>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Text id="basic-addon1">Email</InputGroup.Text>
+                    <FormControl
+                      placeholder="email@domain.com"
+                      value={email}
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                      }}
+                    />
+                  </InputGroup>
+
+                  <Button
+                    variant="info"
+                    block
+                    size="sm"
+                    disabled={busy}
+                    onClick={() => associateAndSend()}
+                  >
+                    <small>Sign And Send</small>
+                  </Button>
+                </> : <>
+                  <div>Email: {email}</div>
+                  <div>Signature: {signature}</div>
+                  <div
+                    style={
+                      status === "Submited successfully" ? { color: "green" } : {}
+                    }
+                  >
+                    Status: {status}
+                  </div>
+                </>
+              }
+              { status === "Submited successfully" &&
+                <Card.Title style={{marginTop: 8}}>
+                  You have been successfully registered in our whitelist!
+                </Card.Title>
+              }
             </Card>
           ) : null}
-        </section>
+        </Container>
       </header>
     </div>
   );
